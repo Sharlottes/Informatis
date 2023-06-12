ed867668c887d1befb33f8cf14671f1239695adb
Sharlotte <aaa9810321@gmail.com>
2023-06-12 16:17:04 +0900

    feat: add `EventEmitter` & `TabsFragment`

---
+++ src/informatis/core/EventEmitter.java
@@ -0,0 +1,44 @@
+package informatis.core;
+
+import arc.struct.*;
+import informatis.SUtils;
+
+public class EventEmitter<T> {
+    ObjectMap<T, ObjectSet<Runnable>> events = new ObjectMap<>();
+
+    public Runnable subscribe(T event, Runnable listener) {
+        ObjectSet<Runnable> listeners = events.get(event);
+        if(listeners == null) events.put(event, ObjectSet.with(listener));
+        else listeners.add(listener);
+        return () -> unsubscribe(event, listener);
+    }
+
+    public void unsubscribe(T event, Runnable listener) {
+        ObjectSet<Runnable> listeners = events.get(event);
+        if(listeners == null) return;
+        listeners.remove(listener);
+    }
+
+    public void unsubscribe(Runnable listener) {
+        ObjectMap.Entries<T, ObjectSet<Runnable>> eventEntries = events.entries();
+        while (eventEntries.hasNext()) {
+            ObjectMap.Entry<T, ObjectSet<Runnable>> eventEntry = eventEntries.next();
+            SUtils.loop(eventEntry.value, (otherListener, i) -> {
+                if(otherListener == listener) {
+                    events.get(eventEntry.key).remove(listener);
+                    return true;
+                }
+                return false;
+            });
+
+        }
+    }
+
+    public void fire(T event) {
+        ObjectSet<Runnable> listeners = events.get(event);
+        if(listeners == null) return;
+        for (Runnable listener : listeners) {
+            listener.run();
+        }
+    }
+}
---
+++ src/informatis/ui/components/TabsFragment.java
@@ -0,0 +1,35 @@
+package informatis.ui.components;
+
+import arc.scene.ui.TextButton;
+import arc.scene.ui.layout.Table;
+import informatis.SUtils;
+import informatis.core.EventEmitter;
+import mindustry.ui.Styles;
+
+public class TabsFragment extends Table {
+    public int currentTabIndex = 0;
+    public EventEmitter<Event> eventEmitter = new EventEmitter<>();
+
+    public TabsFragment(String... tabs) {
+        super();
+        TextButton[] buttons = new TextButton[tabs.length];
+
+        SUtils.loop(tabs.length, i -> {
+            TextButton button = new TextButton(tabs[i], Styles.flatToggleMenut);
+            buttons[i] = button;
+            button.clicked(() -> {
+                currentTabIndex = i;
+                button.toggle();
+                SUtils.loop(buttons, (otherButton, bi) -> {
+                    otherButton.setChecked(currentTabIndex == bi);
+                });
+                eventEmitter.fire(Event.TabChanged);
+            });
+            add(button).height(50).growX();
+        });
+    }
+
+    public enum Event {
+        TabChanged
+    }
+}
