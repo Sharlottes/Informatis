126107fee5879918d410ebc5d93e602733eab239
Sharlotte <aaa9810321@gmail.com>
2023-08-21 09:00:30 +0900

    refactor: 중첩 조건문 제거, 좀 더 의미론적이게 개선

--- src/informatis/draws/BlockRangeDraw.java
+++ src/informatis/draws/BlockRangeDraw.java
@@ -14,44 +14,80 @@ public class BlockRangeDraw extends OverDraw {
     public BlockRangeDraw() {
         super("blockRange", OverDrawCategory.Range);
     }
+    private final static BlockRangeData blockRangeData = new BlockRangeData();
+
+    private boolean validate(Building build) {
+        if(!(build instanceof BaseTurret.BaseTurretBuild turret)) return false;
+        if(!isInCamera(build.x, build.y, turret.range() * 2)) return false;
+
+        boolean
+                includeAir = settings.getBool("airRange", true),
+                includeGround = settings.getBool("groundRange", true),
+                includeAlice = settings.getBool("aliceRange", true);
+
+        if(includeAlice && player.team() != build.team) return false;
+
+        blockRangeData.init(turret);
+        if(includeGround && !blockRangeData.canDetectGround()) return false;
+        if(includeAir && !blockRangeData.canDetectAir()) return false;
+
+        return true;
+    }
 
     @Override
     public void onBuilding(Building build) {
-            Unit playerUnit = player.unit();
+        if(!validate(build)) return;
 
-            boolean includeInvalid = settings.getBool("invalidRange", true),
-                includeAir = settings.getBool("airRange", true), isAir = playerUnit == null || playerUnit.isFlying(),
-                includeGround = settings.getBool("groundRange", true), isGround = playerUnit == null || !playerUnit.isFlying(),
-                includeAlice = settings.getBool("aliceRange", true),
-                shader = settings.getBool("RangeShader", true);
+        boolean includeInvalid = settings.getBool("invalidRange", true);
+        boolean canShoot = blockRangeData.canShoot();
+        if(includeInvalid && !canShoot) return;
+        boolean shader = settings.getBool("RangeShader", true);
 
-            if(!((includeAlice || player.team() != build.team)
-                    && build instanceof BaseTurret.BaseTurretBuild turret && isInCamera(build.x, build.y, turret.range() * 2))) return;
+        int index = canShoot ? build.team.id > 5 ? 2 : build.team.id : 0;
+        float range = ((BaseTurret.BaseTurretBuild) build).range();
+        Draw.color(Team.baseTeams[index].color);
+        if (shader) {
+            Draw.z(166 + (Team.baseTeams.length - index) * 3);
+            Fill.poly(build.x, build.y, Lines.circleVertices(range), range);
+        } else {
+            Drawf.dashCircle(build.x, build.y, range, Team.baseTeams[index].color);
+        }
+    }
+}
 
-            boolean valid = false;
-            if (build instanceof Turret.TurretBuild turretBuild) {
-                Turret block = (Turret) turretBuild.block;
-                if ((build.team == player.team()
-                        || (block.targetAir && isAir && includeAir)
-                        || (block.targetGround && isGround && includeGround))
-                        && turretBuild.hasAmmo() && turretBuild.canConsume()) valid = true;
-            } else if (build instanceof TractorBeamTurret.TractorBeamBuild tractorBeamBuild) {
-                TractorBeamTurret block = (TractorBeamTurret) tractorBeamBuild.block;
-                if ((build.team == player.team()
-                        || (block.targetAir && isAir && includeAir)
-                        || (block.targetGround && isGround && includeGround))
-                        && tractorBeamBuild.canConsume()) valid = true;
-            }
+class BlockRangeData {
+    private boolean canDetectGround; // 지상유닛 감지 여부
+    private boolean canDetectAir; // 지상유닛 감지 여부
+    private boolean canShoot; // 실제로 발사가 가능한지
 
-            if(!includeInvalid && !valid) return;
+    public boolean canDetectGround() {
+        return canDetectGround;
+    }
 
-            //non-base teams are considered as crux
-            int index = valid ? build.team.id > 5 ? 2 : build.team.id : 0;
-            float range = turret.range();
-            Draw.color(Team.baseTeams[index].color);
-            if (shader) {
-                Draw.z(166+(Team.baseTeams.length-index)*3);
-                Fill.poly(build.x, build.y, Lines.circleVertices(range), range);
-            } else Drawf.dashCircle(build.x, build.y, range, Team.baseTeams[index].color);
+    public boolean canDetectAir() {
+        return canDetectAir;
+    }
+
+    public boolean canShoot() {
+        return canShoot;
+    }
+
+    public void init(BaseTurret.BaseTurretBuild build) {
+        Unit playerUnit = player.unit();
+
+        boolean isAir = playerUnit == null || playerUnit.isFlying(),
+                isGround = playerUnit == null || !playerUnit.isFlying();
+
+        if (build instanceof Turret.TurretBuild turretBuild) {
+            Turret block = (Turret) turretBuild.block;
+            canDetectAir = block.targetAir;
+            canDetectGround = block.targetGround;
+            canShoot = turretBuild.hasAmmo() && ((canDetectGround && isGround) || (canDetectAir && isAir));
+        } else if (build instanceof TractorBeamTurret.TractorBeamBuild tractorBeamBuild) {
+            TractorBeamTurret block = (TractorBeamTurret) tractorBeamBuild.block;
+            canDetectAir = block.targetAir;
+            canDetectGround = block.targetGround;
+            canShoot = tractorBeamBuild.canConsume() && ((canDetectGround && isGround) || (canDetectAir && isAir));
+        }
     }
 }
