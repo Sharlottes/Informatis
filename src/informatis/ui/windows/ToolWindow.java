b121766f2abadc984377413b8ebd7595103e64e3
Sharlotte <aaa9810321@gmail.com>
2023-08-22 04:20:50 +0900

    aaa

--- src/informatis/ui/windows/ToolWindow.java
+++ src/informatis/ui/windows/ToolWindow.java
@@ -1,8 +1,10 @@
 package informatis.ui.windows;
 
 import arc.Events;
+import arc.graphics.Color;
 import arc.scene.Element;
 import arc.struct.Seq;
+import informatis.SVars;
 import informatis.draws.*;
 import arc.math.geom.*;
 import arc.scene.ui.*;
@@ -16,90 +18,56 @@ import mindustry.ui.*;
 
 import static arc.Core.bundle;
 import static arc.Core.settings;
+import static mindustry.Vars.*;
 
 public class ToolWindow extends Window {
-    Vec2 scrollPos = new Vec2(0, 0);
-    OverDrawCategory selected;
-    float heat;
+    private OverDrawCategory selected = OverDrawCategory.values()[0];
+    private Table mainTable;
 
     public ToolWindow() {
         super(Icon.edit, "tool");
-
-        Events.run(EventType.Trigger.update, () -> {
-            heat += Time.delta;
-            if(heat >= 60f) {
-                heat = 0f;
-                ((ScrollPane) find("tool-pane")).setWidget(rebuild());
-            }
-        });
     }
 
     @Override
     public void buildBody(Table table) {
-        scrollPos = new Vec2(0, 0);
-
-        table.background(Styles.black8)
-            .top()
-            .left();
-        table.table(pane -> {
-            pane.add(new OverScrollPane(rebuild(), Styles.noBarPane, scrollPos).disableScroll(true, false)).name("tool-pane");
-        })
-        .top()
-        .marginLeft(4f)
-        .marginRight(12f);
-        table.table(stats -> {
-            stats.top();
-            stats.add(rebuildStats()).name("tool-stats");
-        }).growY();
+        table.background(Styles.black8).top().left().margin(12)
+                .defaults().growY().top();
+        table.pane(Styles.noBarPane, rebuildSideTab()).scrollY(true)
+                .padRight(2 * 8f).growY();
+        mainTable = table.add(rebuildMain()).growX().get();
     }
 
-    Table rebuild() {
+    Table rebuildSideTab() {
         return new Table(icons -> {
+            icons.top();
             for(OverDrawCategory category : OverDrawCategory.values()) {
-                icons.button(category.icon, () -> {
-                    selected = category;
-                    Table table = find("tool-stats");
-                    table.clearChildren();
-                    table.add(rebuildStats());
-                })
-                .grow()
-                .tooltip(t -> t
-                    .background(Styles.black8)
-                    .add(category.name)
-                    .color(Pal.accent)
-                )
-                .row();
+                final OverDrawCategory catCategory = category;
+                icons.button(catCategory.icon, Styles.clearTogglei, () -> {
+                    selected = catCategory;
+                    mainTable.clearChildren();
+                    mainTable.add(rebuildMain());
+                }).size(iconLarge).checked((x) -> selected == catCategory);
+                icons.row();
             }
         });
     }
 
-    Table rebuildStats() {
+    Table rebuildMain() {
         return new Table(tool -> {
-            if(selected == null) return;
-            tool.top();
-            tool.table(Tex.underline2, label ->
-                label.add(selected.name).color(Pal.accent)
-            ).row();
+            tool.top().left().defaults().growX().left();
+            tool.add(selected.name).color(Pal.accent).labelAlign(Align.left);
+            tool.row();
+            tool.image().color(Color.gray).height(4f).pad(4, 0, 4, 0);
+            tool.row();
             tool.table(desc -> {
-                desc.background(Styles.squarei.up).left();
+                desc.top().left().defaults().left().labelAlign(Align.left);
                 for(OverDraw draw : OverDraws.getDraws().get(selected, new Seq<>())) {
-                    desc.check(bundle.get("setting."+draw.name+".name"), settings.getBool(draw.name), b->settings.put(draw.name, b))
+                    desc.check(bundle.get("setting."+draw.name+".name"), settings.getBool(draw.name), b -> settings.put(draw.name, b))
                         .tooltip(t -> {
                             t.background(Styles.black8).add(bundle.get("setting."+draw.name+".description"));
                         })
-                        .disabled(!selected.enabled)
-                        .left()
-                        .row();
-                }
-            })
-                .name("unit-stats")
-                .left()
-                .row();
-            tool.check("@mod.enable", selected.enabled, c -> {
-                selected.enabled = c;
-                Table table = find("unit-stats");
-                for (Element elem : table.getChildren()) {
-                    if (elem instanceof CheckBox cb) cb.setDisabled(!c);
+                        .disabled(!selected.enabled);
+                    desc.row();
                 }
             });
         });
