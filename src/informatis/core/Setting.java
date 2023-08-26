097fb2fc55b93f3a7ff2188b217e78fa839cbda9
Sharlotte <seeseafood@naver.com>
2023-08-26 19:41:03 +0900

    refactor: barstyle doesn't support anymore

--- src/informatis/core/Setting.java
+++ src/informatis/core/Setting.java
@@ -110,7 +110,6 @@ public class Setting {
 
         Seq<Seq<SharSetting>> settingSeq = new Seq<>();
         Seq<SharSetting> tapSeq = new Seq<>();
-        addGraphicSlideSetting("barstyle", 0, 0, 5, 1, s -> s == 0 ? bundle.get("default-bar") : s + bundle.get("th-bar"), tapSeq);
         addGraphicCheckSetting("schem", !mobile, tapSeq, () -> FragmentManager.quickSchemFragment.rebuildBody());
 
         //TODO: remove all drawing settings
