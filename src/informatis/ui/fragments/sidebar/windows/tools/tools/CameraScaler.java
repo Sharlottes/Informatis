674a01b8aa3162a53845267145d1206c51a39ff8
Sharlotte <seeseafood@naver.com>
2023-08-27 22:04:49 +0900

    fix: 맵 초기 입장 시 초기값에 기능 적용

--- src/informatis/ui/fragments/sidebar/windows/tools/tools/CameraScaler.java
+++ src/informatis/ui/fragments/sidebar/windows/tools/tools/CameraScaler.java
@@ -1,6 +1,8 @@
 package informatis.ui.fragments.sidebar.windows.tools.tools;
 
+import arc.Events;
 import mindustry.Vars;
+import mindustry.game.EventType;
 
 public class CameraScaler extends Tool {
     private float lastMinZoom;
@@ -10,12 +12,20 @@ public class CameraScaler extends Tool {
 
     public CameraScaler() {
         super("camerascaler");
+
+        Events.on(EventType.WorldLoadEndEvent.class, x -> {
+            toggleScaler();
+        });
     }
 
     @Override
     public void setEnabled(boolean value) {
         super.setEnabled(value);
-        if(value) {
+        toggleScaler();
+    }
+
+    private void toggleScaler() {
+        if(isEnabled()) {
             lastMinZoom = Vars.renderer.minZoom;
             Vars.renderer.minZoom = targetMinZoom;
         } else {
--- src/informatis/ui/fragments/sidebar/windows/tools/tools/FogRemover.java
+++ src/informatis/ui/fragments/sidebar/windows/tools/tools/FogRemover.java
@@ -1,19 +1,29 @@
 package informatis.ui.fragments.sidebar.windows.tools.tools;
 
+import arc.Events;
 import arc.graphics.Color;
 import mindustry.Vars;
+import mindustry.game.EventType;
 
 public class FogRemover extends Tool {
     Color lastStaticColor, lastDynamicColor;
 
     public FogRemover() {
         super("fogremover");
+
+        Events.on(EventType.WorldLoadEndEvent.class, x -> {
+            toggleRemover();
+        });
     }
 
     @Override
     public void setEnabled(boolean value) {
         super.setEnabled(value);
-        if(value) {
+        toggleRemover();
+    }
+
+    private void toggleRemover() {
+        if(isEnabled()) {
             lastStaticColor = Vars.state.rules.staticColor;
             lastDynamicColor = Vars.state.rules.dynamicColor;
             Vars.state.rules.staticColor = Color.clear;
