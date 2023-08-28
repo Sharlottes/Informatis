38831a90ac7b296443330cbdb275b879f15a1350
Sharlotte <seeseafood@naver.com>
2023-08-28 23:19:54 +0900

    refactor: split out metadata from entry point

--- src/informatis/Informatis.java
+++ src/informatis/Informatis.java
@@ -1,30 +1,23 @@
 package informatis;
 
-import informatis.core.OverDrawer;
-import informatis.core.Setting;
 import informatis.ui.fragments.sidebar.windows.tools.draws.OverDrawManager;
-import informatis.ui.fragments.FragmentManager;
+import informatis.ui.fragments.sidebar.windows.tools.tools.ToolManager;
 import informatis.ui.fragments.sidebar.dialogs.DialogManager;
 import informatis.ui.fragments.sidebar.windows.*;
-import arc.*;
-import informatis.ui.fragments.sidebar.windows.tools.tools.ToolManager;
-import mindustry.*;
+import informatis.ui.fragments.FragmentManager;
+import informatis.core.ModMetadata;
+import informatis.core.OverDrawer;
+import informatis.core.Setting;
+
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
-
-import static arc.Core.*;
+import arc.*;
 
 public class Informatis extends Mod {
     @Override
     public void init(){
-        Core.app.post(() -> {
-            Mods.ModMeta meta = Vars.mods.locateMod("informatis").meta;
-            meta.displayName = "[#B5FFD9]Informatis[]";
-            meta.author = "[#B5FFD9]Sharlotte[lightgray]#0018[][]";
-            meta.description = bundle.get("shar-description");
-        });
-
         Events.on(ClientLoadEvent.class, e -> {
+            ModMetadata.init();
             Setting.init();
             WindowManager.init();
             DialogManager.init();
